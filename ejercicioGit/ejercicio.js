857b10fd6d40a519e8b976a64cbf6a2f85a95104
David-Llerena <dllerena@espol.edu.ec>
2024-08-07 16:23:44 -0500

    el boton modifica la imagen

--- ejercicioGit/ejercicio.js
+++ ejercicioGit/ejercicio.js
@@ -1,3 +1,7 @@
 saludar=function(){
+    let cmpImagenSaludo;    
+    cmpImagenSaludo=document.getElementById("imgsaludo");
+    cmpImagenSaludo.src="./imagenes/perrito.gif"
     console.log("Ingresa a saludar");
+
 }
