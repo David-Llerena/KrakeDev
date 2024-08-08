6baf70f1a2d070dbaebd6b003fc8b687f2452d65
David-Llerena <dllerena@espol.edu.ec>
2024-08-08 15:52:43 -0500

    Cambios para registrar edad y estatura

--- utilitarios/saludo.js
+++ utilitarios/saludo.js
@@ -2,6 +2,8 @@ saludar=function(){
     //recuperar el valor de la caja de texto
     let nombre=recuperarTexto("txtNombre");
     let apellido=recuperarTexto("txtApellido");
+    let edad=recuperarInt("txtEdad");
+    let estatura=recuperarFloat("txtEstatura");
 }
 recuperarTexto=function(idComponente){
     let componente;
@@ -9,4 +11,14 @@ recuperarTexto=function(idComponente){
     componente=document.getElementById(idComponente);
     valorIngresado=componente.value;
     return valorIngresado;
+}
+recuperarInt=function(idComponente){
+    let valorCaja=recuperarTexto(idComponente);
+    let valorEntero=parseInt(valorCaja);
+    return valorEntero;
+}
+recuperarFloat=function(idComponente){
+    let valorCaja=recuperarTexto(idComponente);
+    let valorFlotante=parseFloat(valorCaja);
+    return valorFlotante;
 }
