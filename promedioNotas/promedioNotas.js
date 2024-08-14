c6fa3619a879fe2448b4b8324a674fd63f8ee15b
David-Llerena <dllerena@espol.edu.ec>
2024-08-14 10:46:07 -0500

    Se agrega el archivo de promedioNotas que es el reto 29

---
+++ promedioNotas/promedioNotas.js
@@ -0,0 +1,15 @@
+calcularPromedioNotas=function(){
+    let nota1;
+    let nota2;
+    let nota3;
+    let promedio;
+
+    nota1=recuperarFlotante("txtNota1");
+    nota2=recuperarFlotante("txtNota2");
+    nota3=recuperarFlotante("txtNota3");
+
+    promedio=calcularPromedio(nota1,nota2,nota3);
+
+    cambiarTexto("lblPromedio",promedio.toFixed(2));
+
+}
---
+++ promedioNotas/serviciosPromedioNotas.js
@@ -0,0 +1,5 @@
+calcularPromedio=function(n1,n2,n3){
+    let valorSuma= n1+n2+n3;
+    let valorPromedio = valorSuma/3;
+    return valorPromedio;
+}
