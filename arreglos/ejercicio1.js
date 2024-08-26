aa1a89fae0868e09eb187c57579814666a1cb785
David-Llerena <dllerena@espol.edu.ec>
2024-08-26 11:15:39 -0500

    Se agrega ejercicio de arreglos

--- arreglos/ejercicio1.js
+++ arreglos/ejercicio1.js
@@ -19,6 +19,7 @@ probarAgregar=function(){
 }
 agregarNota =function(nota){
     notas.push(nota);
+    mostrarNotas();
 }
 
 calcularPromedio=function(){
@@ -35,4 +36,24 @@ ejecutarPromedio=function(){
     notaRecuperada=calcularPromedio();
     notaRecuperada=parseInt(notaRecuperada);
     mostrarTexto("lblPromedio", notaRecuperada);
+}
+generarTabla=function(){
+    let contenidoTabla="";
+    let cmpTabla=document.getElementById("divTabla");
+    contenidoTabla+="<table> <tr><td>UNO</td></tr>"+
+    "<td>DOS</td></tr></table>";
+    cmpTabla.innerHTML=contenidoTabla;
+}
+mostrarNotas=function(){
+    let cmpTabla=document.getElementById("divTabla");
+    let contenidoTabla="<table><tr><th>NOTA</th></tr>";
+    let miNota;
+    for(let i=0; i<notas.length;i++){
+        miNota=notas[i];
+        contenidoTabla+="<tr><td>";
+        contenidoTabla+=miNota;
+        contenidoTabla+="</tr></td>";
+    }
+    contenidoTabla+="</table>";
+    cmpTabla.innerHTML=contenidoTabla;
 }
