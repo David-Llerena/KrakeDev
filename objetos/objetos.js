b5e023f42f5c51481eeab22febed1448834e5e50
David-Llerena <dllerena@espol.edu.ec>
2024-08-27 21:12:02 -0500

    Se agrega reto 43 crear producto

--- objetos/objetos.js
+++ objetos/objetos.js
@@ -14,3 +14,24 @@ probarAtributos=function(){
         console.log("Si esta vivo");
     }
 }
+crearProducto=function(){
+    let producto1={
+        nombre:"Manzana",
+        precio: 2.50,
+        stock: 5,
+    }
+    let producto2={
+        nombre:"Pera",
+        precio: 2.75,
+        stock: 6,
+    }
+    console.log(producto1.nombre);
+    console.log(producto2.precio);
+    if(producto1.stock>producto2.stock){
+        console.log("Producto 1 tiene mayor stock");
+    }else if(producto1.stock<producto2.stock){
+        console.log("Producto 2 tiene mayor stock");
+    }else{
+        console.log("Ambos productos tiene el mismo stock")
+        }
+}
