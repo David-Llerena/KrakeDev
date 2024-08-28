07a39bce5c371da6e64e1a39d10c4df4bc0556e0
David-Llerena <dllerena@espol.edu.ec>
2024-08-28 12:06:08 -0500

    Se agrega Ejercicio Objetos

--- objetos/objetos.js
+++ objetos/objetos.js
@@ -16,7 +16,7 @@ probarAtributos=function(){
 }
 crearProducto=function(){
     let producto1={
-        nombre:"Manzana",
+        nombre:"Manzana", 
         precio: 2.50,
         stock: 5,
     }
@@ -34,4 +34,62 @@ crearProducto=function(){
     }else{
         console.log("Ambos productos tiene el mismo stock")
         }
+}
+modificarAtributos=function(){
+    let cuenta={
+        numero:"5147895278",
+        saldo:0.0
+    }
+    cuenta.saldo=100;
+    cuenta.saldo+=10;
+    console.log(cuenta.saldo);
+}
+crearCliente=function(){
+    let cliente={
+        cedula: "0926324237",
+        nombre:"David"
+    }
+    let cliente1={
+    }
+    cliente1.nombre="Romeo";
+    cliente1.apellido="Salcedo";
+    cliente1.cedula="123456789";
+}
+probarIncrementarSaldo=function(){
+    let cta={
+        numero:"123456",
+        saldo:34.0
+        }
+        incrementarSaldo(cta,100);
+        console.log(cta.saldo);
+} 
+probarDeterminarMayor=function(){
+    let per1={
+        nombre:"Daniel",
+        edad:45
+    }
+    let per2={
+        nombre:"Luisa",
+        edad:48
+    }
+    let mayor;
+    mayor=determinarMayor(per1,per2);
+    if(mayor!=null){
+        console.log("El mayor es: "+ mayor.nombre);
+    }
+}
+
+
+incrementarSaldo=function(cuenta,monto){
+    cuenta.saldo=+monto;
+
+}
+determinarMayor=function(persona1,persona2){
+     if(persona1.edad>persona2.edad){
+        return persona1;
+     }else if(persona2.edad>persona1.edad){
+        return persona2;
+     }else{
+        return null;
+     }
 }
